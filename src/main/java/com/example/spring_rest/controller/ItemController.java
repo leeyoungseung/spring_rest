bcf3e66fc1fe0995fae090c5b5a7611e933ef4a6
leeyoungseung <leeyoungseung@DESKTOP-QG5IT2R>
2019-09-27 00:28:38 +0900

    recently modified

--- src/main/java/com/example/spring_rest/controller/ItemController.java
+++ src/main/java/com/example/spring_rest/controller/ItemController.java
@@ -2,6 +2,8 @@ package com.example.spring_rest.controller;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,9 @@ import com.example.spring_rest.service.ItemService;
 @RestController
 @RequestMapping(path = "/api")
 public class ItemController {
+	
+	private static final Logger log = LogManager.getLogger(ItemController.class);
+	
     @Autowired
     ItemService itemService;
     
